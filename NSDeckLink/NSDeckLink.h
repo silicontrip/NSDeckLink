be322196feb7a6241693ddc6d572563671a65f45
mark heath (@somnio) <silicontrip@gmail.com>
2022-04-11 12:54:57 +1000

    Move complete.

---
+++ NSDeckLink/NSDeckLink.h
@@ -0,0 +1,18 @@
+//
+//  NSDeckLink.h
+//  NSDeckLink
+//
+//  Created by Mark Heath on 11/4/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSDeckLink.
+FOUNDATION_EXPORT double NSDeckLinkVersionNumber;
+
+//! Project version string for NSDeckLink.
+FOUNDATION_EXPORT const unsigned char NSDeckLinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSDeckLink/PublicHeader.h>
+
+
