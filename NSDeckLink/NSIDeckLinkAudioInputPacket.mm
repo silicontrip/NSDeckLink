466e6ccd63239e87124ea3e0bf67bc7e6d28a341
Mark Heath <silicontrip@gmail.com>
2024-12-10 14:46:30 +1100

    check input packet for null. Return copy of Bytes.

--- NSDeckLink/NSIDeckLinkAudioInputPacket.mm
+++ NSDeckLink/NSIDeckLinkAudioInputPacket.mm
@@ -5,7 +5,10 @@
 + (NSIDeckLinkAudioInputPacket*)audioInputPacketWithIDeckLinkAudioInputPacket:(IDeckLinkAudioInputPacket *)packet {
 	return [[[NSIDeckLinkAudioInputPacket alloc] initWithIDeckLinkAudioInputPacket:packet] autorelease];
 }
+
 - (instancetype)initWithIDeckLinkAudioInputPacket:(IDeckLinkAudioInputPacket *)packet {
+	if (packet == NULL)
+		return nil;
 	if (self = [super initWithIUnknown:packet refiid:IID_IDeckLinkAudioInputPacket]) {
 		_audioinputpacket = packet;
 	}
@@ -32,9 +35,19 @@
 	void* data;
 	if ( _audioinputpacket->GetBytes(&data) != S_OK) 
 		return nil;
-		
-	NSInteger dlen = _audioinputpacket->GetSampleFrameCount();
-	return [NSData dataWithBytesNoCopy:data length:dlen];
+
+	// a frame is a single point in time.
+	// it does not consider bits per sample
+	// and it doesn't consider number of channels
+
+	// calculate sample size
+
+	// TODO: correctly calculate sampleSize.
+	NSUInteger sampleSize = 4;
+
+	NSInteger dlen = _audioinputpacket->GetSampleFrameCount() * sampleSize;
+	//cannot use dataWithBytesNoCopy as NSData will attempt to free the buffer when released but the Decklink API will also free it.
+	return [NSData dataWithBytes:data length:dlen];
 	
 }
 
